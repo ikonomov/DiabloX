aaa1f6794baa875c78206c516e6fef6b5791958d
ikonomov <pikonomov@gmail.com>
2023-01-27 00:05:27 -0600

    Update discord.cpp

--- Source/discord/discord.cpp
+++ Source/discord/discord.cpp
@@ -25,7 +25,7 @@ namespace devilution {
 namespace discord_manager {
 
 // App ID used for DevilutionX's Diablo (classic Diablo's is 496571953147150354)
-constexpr discord::ClientId DiscordDevilutionxAppId = 795760213524742205;
+constexpr discord::ClientId DiscordDevilutionxAppId = 937153467028475935;
 
 constexpr auto IgnoreResult = [](discord::Result result) {};
 

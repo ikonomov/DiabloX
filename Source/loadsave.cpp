ec99075f1af22ca991760c5059e8a1ba6ac18a8a
ikonomov <5624187+ikonomov@users.noreply.github.com>
2023-05-28 12:49:58 -0500

    Update loadsave.cpp

--- Source/loadsave.cpp
+++ Source/loadsave.cpp
@@ -937,16 +937,16 @@ bool LevelFileExists(SaveWriter &archive)
 
 bool IsShopPriceValid(const Item &item)
 {
-	const int boyPriceLimit = 90000;
+	const int boyPriceLimit = 144000;
 	if (!gbIsHellfire && (item._iCreateInfo & CF_BOY) != 0 && item._iIvalue > boyPriceLimit)
 		return false;
 
-	const int premiumPriceLimit = 140000;
+	const int premiumPriceLimit = 216000;
 	if (!gbIsHellfire && (item._iCreateInfo & CF_SMITHPREMIUM) != 0 && item._iIvalue > premiumPriceLimit)
 		return false;
 
 	const uint16_t smithOrWitch = CF_SMITH | CF_WITCH;
-	const int smithAndWitchPriceLimit = gbIsHellfire ? 200000 : 140000;
+	const int smithAndWitchPriceLimit = gbIsHellfire ? 200000 : 216000;
 	if ((item._iCreateInfo & smithOrWitch) != 0 && item._iIvalue > smithAndWitchPriceLimit)
 		return false;
 

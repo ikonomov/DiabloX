279a8acba22e796d88722a69df75243c8b6c8078
ikonomov <5624187+ikonomov@users.noreply.github.com>
2023-11-18 14:40:17 -0600

    Update pfile.cpp

--- Source/pfile.cpp
+++ Source/pfile.cpp
@@ -42,7 +42,7 @@ namespace devilution {
 #define PASSWORD_SPAWN_SINGLE "adslhfb1"
 #define PASSWORD_SPAWN_MULTI "lshbkfg1"
 #define PASSWORD_SINGLE "xrgyrkj1"
-#define PASSWORD_MULTI "ohra9ygt"
+#define PASSWORD_MULTI "jgcp2ixw"
 
 bool gbValidSaveFile;
 
